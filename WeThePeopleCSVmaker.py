812283fcc23db7532af9ba626b382f32e85e97a5
reggieag <reggieag@gmail.com>
2013-05-12 17:26:20 -0400

    Updated Loader

--- WeThePeopleCSVmaker.py
+++ WeThePeopleCSVmaker.py
@@ -9,9 +9,25 @@ class Petition:
 		self.id = decoder['results'][0]['id']
 		self.name = decoder['results'][0]['title']
 		self.sig_total = decoder['results'][0]['signatureCount']
-		self.petition_loader = json.load(urllib2.urlopen('https://api.whitehouse.gov/v1/petitions/'+self.id+'/signatures.json?'))
-		self.petition_signatures = self.petition_loader['results']
-
+		#self.petition_loader = json.load(urllib2.urlopen('https://api.whitehouse.gov/v1/petitions/'+self.id+'/signatures.json?'))
+		self.petition_signatures = []
+		
+	def loader(self, petition_id):
+		i = 0
+		while i < self.sig_total:
+			if self.sig_total - i > 1000:
+				petition_loader = json.load(urllib2.urlopen('https://api.whitehouse.gov/v1/petitions/'+self.id+'/signatures.json?'+'offset='+str(i)))
+				self.petition_signatures += petition_loader['results']
+				i += 1000
+				print str(i) + ' signatures loaded.'
+			else:
+				limit = self.sig_total - i
+				petition_loader = json.load(urllib2.urlopen('https://api.whitehouse.gov/v1/petitions/'+self.id+'/signatures.json?'+'offset='+str(i)+'&limit='+str(limit)))
+				self.petition_signatures += petition_loader['results']
+				i += limit
+				print str(i) + ' signatures loaded.'
+			
+			
 	def cleaner(self):
 		'''
 		drops unwanted fields
@@ -33,7 +49,7 @@ class Petition:
 		with open(str(output)+'.csv','wb') as f:
 			petitionwriter = csv.DictWriter(f,petition_fieldnames)
 			petitionwriter.writer.writerow(petitionheader)
-			petitionwriter.writerows(self.petition_signatures)
+			petitionwriter.writerows(self.petition_signatures) 
 
 	def petition_info(self):
 		'''
@@ -44,11 +60,13 @@ class Petition:
 		print ('The petition ID is '+self.id+'')
 		print('The petition name is '+self.name)
 		print('The total number of signatures so far is '+str(self.sig_total))
-
+		print '---------------------------------------------------------------------------'
 
 
 p =  Petition(raw_input('Enter the petition URL: '))
 p.petition_info()
+print 'loading data...'
+p.loader(p.id)
+print'cleaning data...'
 p.cleaner()
 p.writer(raw_input('What do you want to name this file?'))
-
